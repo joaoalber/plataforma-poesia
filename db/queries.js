59c3fd499ac8eaf07bcfe01f790adbd51e31bf4b
joaoalber <jooalberto1@gmail.com>
2019-09-25 23:18:40 -0300

    CRUD feito sem as views

--- db/queries.js
+++ db/queries.js
@@ -5,6 +5,15 @@ module.exports = {
         create: function(usuario) {
             knex('usuario').insert({nome: usuario.nome, email: usuario.email, 
             sobrenome: usuario.sobrenome, senha: usuario.senha}).then(()=>{});
+        },
+        delete: function(id) {
+            knex('usuario').where('id', id).delete();
+        },
+        readAll: function() {
+            return knex('usuario');
+        },
+        readOne: function(id) {
+            return knex('usuario').where('id', id);
         }
     }
 }
