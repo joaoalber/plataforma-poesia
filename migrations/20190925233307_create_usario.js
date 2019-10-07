3467fde369a24a6b5a948e473fb3d3dec6ae153c
joaoalber <jooalberto1@gmail.com>
2019-10-07 20:19:05 -0300

    Criação table post e poesia

--- migrations/20190925233307_create_usario.js
+++ migrations/20190925233307_create_usario.js
@@ -1,14 +1,36 @@
-exports.up = function(knex) {
+exports.up = function (knex) {
    return knex.schema.createTable("usuario", (table) => {
-      table.increments();
+      table.increments("id");
       table.string("nome").notNullable();
       table.string("sobrenome").notNullable();
       table.string("email").notNullable();
       table.string("senha").notNullable();
-      table.timestamps(true, true);
+      table.timestamps();
+   }).then(() => {
+      return knex.schema.createTable("poesia", (table) => {
+         table.increments("id");
+         table.integer("autor").unsigned().notNullable();
+         table.string("titulo", 30).notNullable();
+         table.string("conteudo").notNullable();
+         table.timestamps()
+
+         table.foreign('autor').references('id').inTable('usuario'); // fk
+      })
+   }).then(() => {
+      return knex.schema.createTable("post", (table) => {
+         table.increments("id");
+         table.integer("autor").unsigned().notNullable();
+         table.string("conteudo").notNullable();
+         table.timestamps();
+
+         table.foreign('autor').references('id').inTable('usuario');
+      })
    })
+      
+      
+
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
    return knex.schema.dropTable("usuario");
 };
